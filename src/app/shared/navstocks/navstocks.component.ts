b0995c810f55e5d88ef88286609c3dc8217676a1
BruceKhetta <pietro.kappa@hotmail.com>
2020-03-12 23:38:13 +0100

    stock navbar is now completed, still bug on first load

--- src/app/shared/navstocks/navstocks.component.ts
+++ src/app/shared/navstocks/navstocks.component.ts
@@ -8,23 +8,32 @@ import { DomainService } from 'src/core/service/domain.service';
 })
 export class NavstocksComponent implements OnInit {
 
-  public company: any[];
-  public price: any[];
+  public company: Array<any> = [];
+  public price: Array<any> = [];
+
+  public generic: Array<any> = [];
 
   constructor(public domainService: DomainService) { }
 
   ngOnInit() {
     this.domainService.getStock(window.sessionStorage.getItem("token")).subscribe(res => {
       console.log("res.response ", res.response);
-      for (var k in res.response) {
-        //this.company.push(k);
+      /*for (var k in res.response) {
+        this.company.push(k);
         for (var y in res.response[k]) {
           if (y == "price") {
             console.log(k, res.response[k][y]);
-            //this.price.push(res.response[k][y]);
+            this.price.push(res.response[k][y]);
           }
         }
+        console.log("company: ", this.company);
+        console.log("price: ", this.price);
+      }*/
+
+      for (let k in res.response) {
+        this.generic.push(res.response[k]);
       }
+      console.log("Generic: ", this.generic)
     });
   }
 
